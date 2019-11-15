804c646d342d0efe4477aafec75cfe87a8778d14
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-15 09:19:31 +0100

    Created helper models

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,27 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+  //   user.password = hash;
+  const newUser = {
+    username,
+    password: hash
+  };
+
+  Users.add(newUser)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
