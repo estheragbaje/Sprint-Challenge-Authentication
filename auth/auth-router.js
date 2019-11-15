b89f4e28c6b3026d067a6ce3068f32b2fd84481f
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-15 09:27:10 +0100

    Created login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,7 +23,24 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
